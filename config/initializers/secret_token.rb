cf79f6438d45d3a85a820d7c6e65dc61b856258d
Kyle Maxwell <kyle@kylemaxwell.com>
2010-11-30 08:23:54 -0800

    skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Docsy::Application.config.secret_token = '5f295859b5a98af9f20f49eb0c09275c176c5870652912fc3c675a04886d4c11e2f703130f13f8051cefbba7eb4c9a309244bc7d37e5aa6d10281281b2f74a40'
